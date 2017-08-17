9c98132fb56a3f6d7df461184faa824d45b1a070
mba <mba@example.com>
2017-08-17 14:29:45 +0100

    Create new react components

---
+++ client/components/App/PostMessage.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import AuthHeader from './AuthHeader';
+import SideBar from './SideBar';
+import '../../assets/main.scss';
+import '../../assets/scripts.js';
+
+export default class PostMessage extends React.Component {
+  render() {
+    return(
+      <div>
+        <AuthHeader />
+        <SideBar />
+          <div className="container group-container">
+    				<div className="row">
+    					<div className="col s12 m8 offset-m2 l9 offset-l3">
+    						<h2>Post a Message</h2>
+    						<p>Post a message to everyone in your group</p>
+    						<div className="row">
+    							<form>
+    								<div className="input-field col s12">
+    									<input className="validate" type="text" name="name" id="name" required placeholder="e.g Hi"/>
+    									<label htmlFor="name">Message</label>
+    								</div>
+    								<div className="input-field col s12">
+    							    <select multiple>
+    							      <option value="" disabled value></option>
+    							      <option value="1">Andela</option>
+    							      <option value="2">Andela-BootCamp</option>
+    							      <option value="3">General</option>
+    							    </select>
+    									<label>Group Name(s)</label>
+    							  </div>
+    								<div className="col l7 offset-l5 button">
+    									<button className="btn-large" type="button">Cancel</button>
+    									<button className="btn-large" type="submit" name="action">Post Message</button>
+    								</div>
+    							</form>
+    						</div>
+    					</div>
+    				</div>
+    			</div>
+      </div>
+    );
+  }
+}
--- client/components/Routes.js
+++ client/components/Routes.js
@@ -3,13 +3,21 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // import components
 import MessageBoard from './App/MessageBoard';
+import PostMessage from './App/PostMessage';
+import CreateGroup from './App/CreateGroup';
+import SignIn from './App/SignIn';
+import ResetPassword from './App/ResetPassword';
 
 export default class Routes extends React.Component {
   render() {
     return(
       <Router>
         <div>
-          <Route exact path={'/'} component={MessageBoard}/>
+          <Route exact path={'/'} component={MessageBoard} />
+          <Route path={'/postmessage'} component={PostMessage} />
+          <Route path={'/creategroup'} component={CreateGroup} />
+          <Route path={'/signin'} component={SignIn} />
+          <Route path={'/resetpassword'} component={ResetPassword} />
         </div>
       </Router>
     );
