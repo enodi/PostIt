4b6104ca0dc105c2f092a28450ff839a8be20dc4
mba <mba@example.com>
2017-08-17 10:44:27 +0100

    Modified routes

--- client/components/Routes.js
+++ client/components/Routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 // import components
-import MessageBoard from './Group/MessageBoard';
+import MessageBoard from './App/MessageBoard';
 
 export default class Routes extends React.Component {
   render() {
