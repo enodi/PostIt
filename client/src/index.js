bb5504ec7c3b5d2185d8f286e8d3375a808a232b
mba <mba@example.com>
2017-08-23 07:14:05 +0100

    Add reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import Routes from './components/Routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+const store = createStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>, document.getElementById('root'));
 registerServiceWorker();
 
 if (module.hot) {
